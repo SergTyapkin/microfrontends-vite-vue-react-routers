f8f027932a8b7d43ebf23ba90d839d1772af037c
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-09-28 06:50:17 +0300

    Починил props для React-child-app

--- vue-host-app/src/components/ReactComponentTemplate.vue
+++ vue-host-app/src/components/ReactComponentTemplate.vue
@@ -38,7 +38,7 @@ export default {
           this.$router.push(nextFullPath);
         }
       },
-    });
+    }, this.elementProps);
     this.navigateReactElement = onParentNavigate;
     this.unmount = unmount;
 
