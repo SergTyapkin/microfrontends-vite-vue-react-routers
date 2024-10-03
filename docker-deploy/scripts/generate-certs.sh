ee4250875f6a99756c3882294e5d7e691cc0ac11
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-10-03 05:32:25 +0300

    try

--- docker-deploy/scripts/generate-certs.sh
+++ docker-deploy/scripts/generate-certs.sh
@@ -1,5 +1,9 @@
-cd docker-deploy || exit
-docker compose --env-file "$1" --env-file ../.env down
-docker compose --env-file "$1" --env-file ../.env up -d nginx-certbot
-docker compose --env-file "$1" --env-file ../.env run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$(. "$1"; echo "$DOMAIN_URL")"
-sudo chmod ugo+rwx -R ./certbot-"$1"/
+if [ "$(. "docker-deploy/$1"; echo "$ENABLE_HTTPS")" = "true" ]; then
+  cd docker-deploy || exit
+  docker compose --env-file "$1" --env-file ../.env down
+  docker compose --env-file "$1" --env-file ../.env up -d nginx-certbot
+  docker compose --env-file "$1" --env-file ../.env run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$(. "$1"; echo "$DOMAIN_URL")"
+  sudo chmod ugo+rwx -R ./certbot-"$1"/;
+else
+  echo "If you want to get certificates, you must set ENABLE_HTTPS in '$1' to 'true' value firstly.";
+fi
--- docker-deploy/scripts/setup-auto-renewing-certs.sh
+++ docker-deploy/scripts/setup-auto-renewing-certs.sh
@@ -1,10 +1,14 @@
-sudo apt-get update
-sudo apt-get install cron
-echo "----------------------------------------------------------------------------------------------"
-echo "Now you must add this string-command in your crontab to auto executing it each 1 month. Command:"
-echo ""
-echo "0 0 1 * * cd $(pwd) && make renew-certs ENV_FILE_NAME=$1 >> ./certbot-renew-$1.log"
-echo ""
-echo "Copy that command and [press Enter]. Then add this string in end of opened file."
-read ENTER
-crontab -e
+if [ "$(. "docker-deploy/$1"; echo "$ENABLE_HTTPS")" = "true" ]; then
+  sudo apt-get update
+  sudo apt-get install cron
+  echo "----------------------------------------------------------------------------------------------"
+  echo "Now you must add this string-command in your crontab to auto executing it each 1 month. Command:"
+  echo ""
+  echo "0 0 1 * * cd $(pwd) && make renew-certs ENV_FILE_NAME=$1 >> ./certbot-renew-$1.log"
+  echo ""
+  echo "Copy that command and [press Enter]. Then add this string in end of opened file."
+  read ENTER
+  crontab -e;
+else
+  echo "If you want to get certificates, you must set ENABLE_HTTPS in '$1' to 'true' value firstly.";
+fi
